fc4bb0360d2d4c9b3052ad46a853f91947680a12
wnsghks <ashi0@naver.com>
2020-07-06 15:58:53 +0900

    Test Commit

--- Assets/Script/NewBehaviourScript.cs
+++ Assets/Script/NewBehaviourScript.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    Transform myTransform;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myTransform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log("Hello JunHwan!");
+        myTransform.Rotate( 0.0f, 100.0f * Time.deltaTime, 0.0f );
     }
 }
