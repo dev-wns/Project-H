48dd518ce83cbe77d39d30040be0f6003c61afc7
TaehongKim <walckm11@gmail.com>
2020-07-12 14:40:01 +0900

    애니메이션 컨트롤러 교체, 시작 팝업창 제거

--- Assets/_3rdPersonController/Scripts/CharacterController/vThirdPersonAnimator.cs
+++ Assets/_3rdPersonController/Scripts/CharacterController/vThirdPersonAnimator.cs
@@ -15,49 +15,52 @@ namespace Invector.vCharacterController
 
         public virtual void UpdateAnimator()
         {
-            if (animator == null || !animator.enabled) return;
-
-            animator.SetBool(vAnimatorParameters.IsStrafing, isStrafing); ;
-            animator.SetBool(vAnimatorParameters.IsSprinting, isSprinting);
-            animator.SetBool(vAnimatorParameters.IsGrounded, isGrounded);
-            animator.SetFloat(vAnimatorParameters.GroundDistance, groundDistance);
-
-            if (isStrafing)
+            if ( animator == null || animator.enabled == false )
             {
-                animator.SetFloat(vAnimatorParameters.InputHorizontal, stopMove ? 0 : horizontalSpeed, strafeSpeed.animationSmooth, Time.deltaTime);
-                animator.SetFloat(vAnimatorParameters.InputVertical, stopMove ? 0 : verticalSpeed, strafeSpeed.animationSmooth, Time.deltaTime);
+                return;
+            }
+
+            animator.SetBool( vAnimatorParameters.IsStrafing, isStrafing ); ;
+            animator.SetBool( vAnimatorParameters.IsSprinting, isSprinting );
+            animator.SetBool( vAnimatorParameters.IsGrounded, isGrounded );
+            animator.SetFloat( vAnimatorParameters.GroundDistance, groundDistance );
+
+            if ( isStrafing == true )
+            {
+                animator.SetFloat( vAnimatorParameters.InputHorizontal, stopMove == true ? 0 : horizontalSpeed, strafeSpeed.animationSmooth, Time.deltaTime );
+                animator.SetFloat( vAnimatorParameters.InputVertical, stopMove == true ? 0 : verticalSpeed, strafeSpeed.animationSmooth, Time.deltaTime );
             }
             else
             {
-                animator.SetFloat(vAnimatorParameters.InputVertical, stopMove ? 0 : verticalSpeed, freeSpeed.animationSmooth, Time.deltaTime);
+                animator.SetFloat( vAnimatorParameters.InputVertical, stopMove == true ? 0 : verticalSpeed, freeSpeed.animationSmooth, Time.deltaTime );
             }
 
-            animator.SetFloat(vAnimatorParameters.InputMagnitude, stopMove ? 0f : inputMagnitude, isStrafing ? strafeSpeed.animationSmooth : freeSpeed.animationSmooth, Time.deltaTime);
+            animator.SetFloat( vAnimatorParameters.InputMagnitude, stopMove == true ? 0f : inputMagnitude, isStrafing == true ? strafeSpeed.animationSmooth : freeSpeed.animationSmooth, Time.deltaTime );
         }
 
-        public virtual void SetAnimatorMoveSpeed(vMovementSpeed speed)
+        public virtual void SetAnimatorMoveSpeed( vMovementSpeed speed )
         {
-            Vector3 relativeInput = transform.InverseTransformDirection(moveDirection);
-            verticalSpeed = relativeInput.z;
-            horizontalSpeed = relativeInput.x;
+            Vector3 relativeInput = transform.InverseTransformDirection( moveDirection );
 
-            var newInput = new Vector2(verticalSpeed, horizontalSpeed);
+            float additionalSpeed = ( isSprinting == true ? 0.5f : 0.0f );
+            verticalSpeed = relativeInput.z + additionalSpeed;
+            horizontalSpeed = relativeInput.x + additionalSpeed;
 
-            if (speed.walkByDefault)
-                inputMagnitude = Mathf.Clamp(newInput.magnitude, 0, isSprinting ? runningSpeed : walkSpeed);
-            else
-                inputMagnitude = Mathf.Clamp(isSprinting ? newInput.magnitude + 0.5f : newInput.magnitude, 0, isSprinting ? sprintSpeed : runningSpeed);
+            var newInput = new Vector2( verticalSpeed, horizontalSpeed );
+
+            inputMagnitude = ( speed.walkByDefault == true ? walkSpeed : runningSpeed ) + additionalSpeed;
+            inputMagnitude = Mathf.Clamp( newInput.magnitude, 0, inputMagnitude );
         }
     }
 
     public static partial class vAnimatorParameters
     {
-        public static int InputHorizontal = Animator.StringToHash("InputHorizontal");
-        public static int InputVertical = Animator.StringToHash("InputVertical");
-        public static int InputMagnitude = Animator.StringToHash("InputMagnitude");
-        public static int IsGrounded = Animator.StringToHash("IsGrounded");
-        public static int IsStrafing = Animator.StringToHash("IsStrafing");
-        public static int IsSprinting = Animator.StringToHash("IsSprinting");
-        public static int GroundDistance = Animator.StringToHash("GroundDistance");
+        public static int InputHorizontal = Animator.StringToHash( "InputHorizontal" );
+        public static int InputVertical = Animator.StringToHash( "InputVertical" );
+        public static int InputMagnitude = Animator.StringToHash( "InputMagnitude" );
+        public static int IsGrounded = Animator.StringToHash( "IsGrounded" );
+        public static int IsStrafing = Animator.StringToHash( "IsStrafing" );
+        public static int IsSprinting = Animator.StringToHash( "IsSprinting" );
+        public static int GroundDistance = Animator.StringToHash( "GroundDistance" );
     }
 }
