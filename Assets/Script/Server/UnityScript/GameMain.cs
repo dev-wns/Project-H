efa1ade4d8bd49296b8776a5e65dca7603b7332a
wnsghks <ashi0@naver.com>
2020-07-24 17:09:21 +0900

    채팅UI UGUI비활성화 시키고 OnGUI함수로 교체, 채팅 패킷 주고받기, 매니저스크립트용 빈게임오브젝트 생성

---
+++ Assets/Script/Server/UnityScript/GameMain.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+public class GameMain : MonoBehaviour
+{
+    string inputText;
+    List<string> receivedTexts;
+    NetworkManager networkManager;
+
+    Vector2 currentScrollpos = new Vector2();
+
+    private void Awake()
+    {
+        this.inputText = "";
+        this.receivedTexts = new List<string>();
+        this.networkManager = GameObject.Find( "NetworkManager" ).GetComponent<NetworkManager>(); ;
+    }
+
+    public void OnReceiveChatMessage( string text )
+    {
+        this.receivedTexts.Add( text );
+        this.currentScrollpos.y = float.PositiveInfinity;
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.BeginVertical();
+        currentScrollpos = GUILayout.BeginScrollView( 
+            currentScrollpos,
+            GUILayout.MaxWidth( Screen.width ),
+            GUILayout.MinWidth( Screen.width ),
+            GUILayout.MaxHeight( Screen.height - 100 ),
+            GUILayout.MinHeight( Screen.height - 100 ) );
+
+        foreach(string text in this.receivedTexts )
+        {
+            GUILayout.BeginHorizontal();
+            GUI.skin.label.wordWrap = true;
+            GUILayout.Label( text );
+            GUILayout.EndHorizontal();
+        }
+
+        GUILayout.EndScrollView();
+        GUILayout.EndVertical();
+
+        // Input
+        GUILayout.BeginHorizontal();
+        this.inputText = GUILayout.TextField( this.inputText,
+                         GUILayout.MaxWidth( Screen.width - 100 ),
+                         GUILayout.MinWidth( Screen.width - 100 ),
+                         GUILayout.MaxHeight( 50 ), GUILayout.MinHeight( 50 ) );
+        if ( GUILayout.Button("Send", 
+            GUILayout.MaxWidth(100), GUILayout.MinWidth(100), 
+            GUILayout.MaxHeight( 50 ), GUILayout.MinHeight( 50 ) ) )
+            {
+                Packet msg = Packet.Create( ( short )PROTOCOL.CHAT_MSG_REQ );
+                msg.Push( this.inputText );
+                this.networkManager.Send( msg );
+                this.inputText = "";
+            }
+        GUILayout.EndHorizontal();
+    }
+}
+
---
+++ Assets/Script/Server/UnityScript/NetworkManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour
+{
+    UnityService gameServer;
+    string receivedMessage;
+
+    private void Awake()
+    {
+        this.receivedMessage = "";
+
+        // 네트워크 통신을 위해 UnityService객체를 추가합니다.
+        this.gameServer = gameObject.AddComponent<UnityService>();
+
+        // 상태 변화( 접속, 끊김 등 )를 통보 받을 델리게이트를 설정합니다.
+        this.gameServer.appcallbackOnStatusChanged += OnStatusChanged;
+
+        // 패킷 수신용 델리게이트를 설정합니다.
+        this.gameServer.appcallbackOnMessage += OnMessage;
+    }
+
+    private void Start()
+    {
+        this.gameServer.Connect( "127.0.0.1", 7979 );
+    }
+
+    void OnStatusChanged( NETWORK_EVENT status )
+    {
+        switch(status )
+        {
+            case NETWORK_EVENT.CONNECTED:
+                {
+                    LogManager.log( "On Connected!" );
+                    this.receivedMessage += "On Connected\n";
+
+                    Packet msg = Packet.Create( ( short )PROTOCOL.CHAT_MSG_REQ );
+                    msg.Push( "HELLO!!!" );
+                    this.gameServer.Send( msg );
+                } break;
+            case NETWORK_EVENT.DISCONNECTED:
+                {
+                    LogManager.log( "DisConnected!" );
+                    this.receivedMessage += "DisConnected\n";
+                } break;
+
+            default: break;
+        }
+    }
+
+    void OnMessage( Packet msg )
+    {
+        PROTOCOL protocol_id = ( PROTOCOL )msg.PopProtocolID();
+
+        switch( protocol_id )
+        {
+            case PROTOCOL.CHAT_MSG_ACK:
+                {
+                    string text = msg.PopString();
+                    GameObject.Find( "ServerMain" ).GetComponent<GameMain>().OnReceiveChatMessage( text );
+                } break;
+            default: break;
+        }
+    }
+
+    public void Send( Packet msg )
+    {
+        this.gameServer.Send( msg );
+    }
+}
